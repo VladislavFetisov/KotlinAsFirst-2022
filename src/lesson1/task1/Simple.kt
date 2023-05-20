983c1e75b41fb7c5e58699bcd24f18ed403ef8bf
VladislavFetisov <1>
2023-05-20 14:15:42 +0300

    Revert "track length"
    This reverts commit eb6b3ae10791b5c8a1c805f83bdb13418019103f.

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -95,7 +95,7 @@ fun angleInRadian(deg: Int, min: Int, sec: Int): Double {
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = 2.0
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = 1.0
 
 /**
  * Простая (2 балла)
